f4df130f4a3fafe0ebcbfa724d9d3f7611c37a7f
loganmiller001 <loganmiller001@live.com>
2018-07-19 10:08:03 -0500

    Removed all internal access modifiers in Query class to public.

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -8,142 +8,142 @@ namespace HumaneSociety
 {
     public static class Query
     {
-        internal static object GetPendingAdoptions()
+        public static Adoption GetPendingAdoptions()
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateAdoption(bool v, Adoption adoption)
+        public static void UpdateAdoption(bool v, Adoption adoption)
         {
             throw new NotImplementedException();
         }
 
-        internal static object GetShots(Animal animal)
+        public static object GetShots(Animal animal)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateShot(string v, Animal animal)
+        public static void UpdateShot(string v, Animal animal)
         {
             throw new NotImplementedException();
         }
 
-        internal static void EnterUpdate(Animal animal, Dictionary<int, string> updates)
+        public static void EnterUpdate(Animal animal, Dictionary<int, string> updates)
         {
             throw new NotImplementedException();
         }
 
-        internal static void RemoveAnimal(Animal animal)
+        public static void RemoveAnimal(Animal animal)
         {
             throw new NotImplementedException();
         }
 
-        internal static Species GetSpecies()
+        public static Species GetSpecies()
         {
             throw new NotImplementedException();
         }
 
-        internal static Client GetClient(string userName, string password)
+        public static Client GetClient(string userName, string password)
         {
             throw new NotImplementedException();
         }
 
-        internal static DietPlan GetDietPlan()
+        public static DietPlan GetDietPlan()
         {
             throw new NotImplementedException();
         }
 
-        internal static void AddAnimal(Animal animal)
+        public static void AddAnimal(Animal animal)
         {
             throw new NotImplementedException();
         }
 
-        internal static Employee EmployeeLogin(string userName, string password)
+        public static Employee EmployeeLogin(string userName, string password)
         {
             throw new NotImplementedException();
         }
 
-        internal static Employee RetrieveEmployeeUser(string email, int employeeNumber)
+        public static Employee RetrieveEmployeeUser(string email, int employeeNumber)
         {
             throw new NotImplementedException();
         }
 
-        internal static void AddUsernameAndPassword(Employee employee)
+        public static void AddUsernameAndPassword(Employee employee)
         {
             throw new NotImplementedException();
         }
 
-        internal static bool CheckEmployeeUserNameExist(string username)
+        public static bool CheckEmployeeUserNameExist(string username)
         {
             throw new NotImplementedException();
         }
 
-        internal static Room GetRoom(int animalId)
+        public static Room GetRoom(int animalId)
         {
             throw new NotImplementedException();
         }
 
-        internal static object GetUserAdoptionStatus(Client client)
+        public static object GetUserAdoptionStatus(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static object GetAnimalByID(int iD)
+        public static object GetAnimalByID(int iD)
         {
             throw new NotImplementedException();
         }
 
-        internal static void Adopt(object animal, Client client)
+        public static void Adopt(object animal, Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static void RunEmployeeQueries(Employee employee, string v)
+        public static void RunEmployeeQueries(Employee employee, string v)
         {
             throw new NotImplementedException();
         }
 
-        internal static object RetrieveClients()
+        public static object RetrieveClients()
         {
             throw new NotImplementedException();
         }
 
-        internal static object GetStates()
+        public static object GetStates()
         {
             throw new NotImplementedException();
         }
 
-        internal static void AddNewClient(string firstName, string lastName, string username, string password, string email, string streetAddress, int zipCode, int state)
+        public static void AddNewClient(string firstName, string lastName, string username, string password, string email, string streetAddress, int zipCode, int state)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateClient(Client client)
+        public static void UpdateClient(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateUsername(Client client)
+        public static void UpdateUsername(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateEmail(Client client)
+        public static void UpdateEmail(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateAddress(Client client)
+        public static void UpdateAddress(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateFirstName(Client client)
+        public static void UpdateFirstName(Client client)
         {
             throw new NotImplementedException();
         }
 
-        internal static void UpdateLastName(Client client)
+        public static void UpdateLastName(Client client)
         {
             throw new NotImplementedException();
         }
